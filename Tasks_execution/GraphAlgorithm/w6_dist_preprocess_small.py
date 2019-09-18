a523b4b53b9324101b5696b5cbc6e6bb741087f6
Alexey Dolgikh <adolgich@arkadium.com>
2019-09-18 15:45:03 +0300

    replaced inline preprocessing with using separare class.

--- Tasks_execution/GraphAlgorithm/w6_dist_preprocess_small.py
+++ Tasks_execution/GraphAlgorithm/w6_dist_preprocess_small.py
@@ -49,7 +49,7 @@ class ContractionHierarchiesPreprocessor:
         self.max_rank = 0
         self.contracted = [False] * self.n
         # Levels of nodes for node ordering heuristics
-        level = [0] * self.n
+        self.level = [0] * self.n
 
     def prepocess(self):
         # NODE ORDERING:
@@ -144,102 +144,10 @@ class ContractionHierarchies:
         # Positions of nodes in the node ordering
         self.rank = [0] * n
 
-        # Implement preprocessing here
-        self.prepocess()
+        preprocessor = ContractionHierarchiesPreprocessor(n, self.adj, self.cost, self.rank)
+        preprocessor.prepocess()
         
-    # ================================================================
-
-    def prepocess(self):
-        q = queue.PriorityQueue()
-        max_rank = 0
-
-        contracted = [False] * self.n
-
-        # Levels of nodes for node ordering heuristics
-        level = [0] * self.n
-
-        # NODE ORDERING:
-        # https://www.coursera.org/learn/algorithms-on-graphs/lecture/Abyzw/node-ordering
-
-        # At the end: new edges (augmentation), rank (and level) array is filled out.
-        for v in range(self.n):
-            q.put((-self.INFINITY, v))
-
-        while not q.empty():
-            v = q.get()[1]
-
-            if not contracted[v]:
-                importance = self.recompute_importance(v, level[v], contracted)
-            
-                if q.empty() or importance <= q.queue[0][0]: # or strong "<" ? # q.queue[0][0] means q.top()
-                    self.conract(v, level)
-                    self.rank[v] = max_rank
-                    max_rank += 1
-                    # self.rank[v] is a position in node order (returned by the preprocessing stage)
-                    contracted[v] = True
-                else:            
-                    q.put((importance, v))
-
-    def conract(self, v, level):
-        # Witness Search https://www.coursera.org/learn/algorithms-on-graphs/lecture/WuGFB/witness-search
-        # For each predecessor ui of v run Dij from ui ignoring v.
-        # Stop Dij:
-            # if d(ui, x) > max max [ l(u,v) + l(v,w) - l(w', w) ]    
-            #    or k edges are processed (increase k from 1 to 5 ???)
-
-        for u_i in range(len(self.adj[1][v])): # self.adj[1][v] are predecessors of v.
-            u = self.adj[1][v][u_i]
-            l_u_v = self.cost[1][v][u_i]
-            dist = dijkstra(self.adj[0], self.cost[0], u, 5*len(self.adj[0][v]), v)
-            for w_i in range(len(self.adj[0][v])): # self.adj[0][v] are successors of v.
-                w = self.adj[0][v][w_i]
-                l_v_w = self.cost[0][v][w_i]
-                c = l_u_v + l_v_w
-                if dist[w] > c:
-                    self.add_shortcut(u, w, c)
-
-        for neighbor in self.adj[1][v] + self.adj[0][v]:
-            level[neighbor] = max( level[neighbor], level[v] + 1 )
-
-    def add_shortcut(self, u, v, c):
-        def update(adj, cost, u, v, c):
-            for i in range(len(adj[u])):
-                if adj[u][i] == v:
-                    cost[u][i] = min(cost[u][i], c)
-                    return
-            adj[u].append(v)
-            cost[u].append(c)
-
-        update(self.adj[0], self.cost[0], u, v, c)
-        update(self.adj[1], self.cost[1], v, u, c)
-
-    def recompute_importance(self, v, level, contracted):
-        # https://www.coursera.org/learn/algorithms-on-graphs/lecture/Abyzw/node-ordering
-
-        # EACH of the 4 quantities is necessary!!!
-
-        # !!! TODO:
-        shortcut_count = 0
-        # shortcut_count - number of added shortcuts s(v); how many shorcuts would be added if we contract this node v.
-        #edge_difference = shortcut_count - len(self.adj[0][v]) - len(self.adj[1][v])        
-        edge_difference = 0
-        
-        n_contracted_neighbors = 0 # number of contracted neighbors
-        shortcut_cover = 0 # number of neighbors that we HAVE TO short cut...
-
-        for neighbor in self.adj[1][v] + self.adj[0][v]:
-            if contracted[neighbor]:
-                n_contracted_neighbors += 1
-            if len(self.adj[1][neighbor] + self.adj[0][neighbor]) == 1:
-                shortcut_cover += 1
-        
-        importance = edge_difference + n_contracted_neighbors + shortcut_cover + level # add weights
-
-        return importance
-
-
-    # ================================================================
-
+    
     def clear(self):
         self.bidistance = [[self.INFINITY] * n, [self.INFINITY] * n]
         self.visited = [False] * n
